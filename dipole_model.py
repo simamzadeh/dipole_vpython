5c235616f1a4039be163276973d72a9ffff890d7
simamzadeh <sima.mz246@gmail.com>
2021-03-15 10:58:14 +0000

    Make spheres
    Make sphere objects for positive, negative particles and test position

--- dipole_model.py
+++ dipole_model.py
@@ -1,2 +1,12 @@
 from vpython import *
 
+# Assign constants
+charge = 1
+distance = 1
+
+# Making the particles
+positive = sphere(pos=vector(0,0,0), q = 1, radius=0.2, color=color.cyan)
+
+negative = sphere(pos=vector(1,0,0), q = -1, radius=0.2, color=color.yellow)
+
+test_position = sphere(pos=vector(3,4,0), radius=0.1)
