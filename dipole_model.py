785243ce434960f238d4c0eb2e3f685723c55019
simamzadeh <sima.mz246@gmail.com>
2021-03-15 12:05:03 +0000

    Create test particle function
    Define a function that takes the position vector of a test particle and plots it as a sphere with an arrow to depict direction

--- dipole_model.py
+++ dipole_model.py
@@ -10,18 +10,36 @@ positive_part = sphere(pos=vector(0,0,0), q = 1, radius=0.2, color=color.cyan)
 
 negative_part = sphere(pos=vector(1,0,0), q = -1, radius=0.2, color=color.yellow)
 
-test_position = sphere(pos=vector(3,4,0), radius=0.1)
+# test_particle = sphere(pos=vector(3,4,0), radius=0.1)
+#
+# # Calculate distance
+# r_pos = test_particle.pos - positive_part.pos
+# r_neg = test_particle.pos - negative_part.pos
+#
+# # Calculate electric field
+# elec_field_pos = k * positive_part.q * (r_pos / (mag(r_pos)**3) )
+# elec_field_neg = k * negative_part.q * (r_neg / (mag(r_neg)**3) )
+#
+# total_elec_field = elec_field_pos + elec_field_neg
+#
+# # Make arrow
+# test_arrow = arrow(pos=vector(3,4,0), axis= (1*10**-8)*total_elec_field)
 
-# Calculate distance
-r_pos = test_position.pos - positive_part.pos
-r_neg = test_position.pos - negative_part.pos
+# Define function to plot test particles
 
-# Calculate electric field
-elec_field_pos = k * positive_part.q * (r_pos / (mag(r_pos)**3) )
-elec_field_neg = k * negative_part.q * (r_neg / (mag(r_neg)**3) )
+def plot_test_particle(test_position):
 
-total_elec_field = elec_field_pos + elec_field_neg
+    sphere(pos=test_position, radius=0.1)
 
-# Make arrow
-test_arrow = arrow(pos=vector(3,4,0), axis= (1*10**-8)*total_elec_field)
+    r_pos = test_position - positive_part.pos
+    r_neg = test_position - negative_part.pos
+
+    elec_field_pos = k * positive_part.q * (r_pos / (mag(r_pos) ** 3))
+    elec_field_neg = k * negative_part.q * (r_neg / (mag(r_neg) ** 3))
+
+    total_elec_field = elec_field_pos + elec_field_neg
+
+    arrow(pos=test_position, axis=(1*10**-8)*total_elec_field)
+
+plot_test_particle(vector(3,4,0))
 
